69d6560bc6935f6f1d180181d3f5346e357f3e25
enricoferrer <enrico.santana@sptech.school>
2024-09-25 10:11:26 -0300

    Primeiro projeto java

---
+++ RadixTranningJava/src/main/java/com/radix/api/RadixTranningJavaApplication.java
@@ -0,0 +1,13 @@
+package com.radix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixTranningJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadixTranningJavaApplication.class, args);
+	}
+
+}
